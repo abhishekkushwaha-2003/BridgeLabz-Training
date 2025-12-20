df330c2b10631345d5ac4c53c74ed22291cb9684
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2025-12-20 21:41:32 +0530

    [Abhishek] : Added a program to find simple interest

---
+++ gcr-codebase/arrays/FindSimpleInterset.java
@@ -0,0 +1,41 @@
+// Write a program to input the Principal, Rate, and Time values and calculate Simple Interest.
+
+
+// Create a class FindSimpleInterset to calculate Simple interest
+
+import java.util.Scanner;
+
+	public class FindSimpleInterset{
+		public static void main (String [] args){
+		
+		// Taking input and storing them in variable
+	
+		Scanner sc = new Scanner(System.in);
+	
+		System.out.println("Enter Principal Amount : ");
+	
+		double principalAmount = sc.nextDouble();
+	
+		System.out.println("Enter Rate of Interest : ");
+	
+		double interestRate = sc.nextDouble();
+	
+		System.out.println("Enter Time Duration : ");
+	
+		double timeDuration = sc.nextDouble();
+		
+		calculateSimpleInterest(principalAmount, interestRate, timeDuration);
+		
+		sc.close();
+		
+	
+	}   // Create a calculateSimpleInterest method for calculating simple interest
+	
+		public static void calculateSimpleInterest(double principalAmount, double interestRate, double timeDuration){
+	
+			double interest =  principalAmount * interestRate * timeDuration / 100;
+	   
+			System.out.print("The Simple Interest is "+interest+" for Principal "+principalAmount+" , Rate of Interest "+interestRate+" and Time "+timeDuration+" year");
+	
+	}
+}
