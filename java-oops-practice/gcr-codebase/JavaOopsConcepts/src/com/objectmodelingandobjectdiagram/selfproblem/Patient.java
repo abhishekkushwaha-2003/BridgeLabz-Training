1e17f3bcb409060476a7149b66245ef170469e91
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2025-12-31 13:27:49 +0530

    [Abishek] : Added a program of Patient class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelingandobjectdiagram/selfproblem/Patient.java
@@ -0,0 +1,13 @@
+package com.objectmodelingandobjectdiagram.selfproblem;
+
+public class Patient {
+
+    // patient name
+    String patientName;
+
+    // constructor
+    Patient(String patientName) {
+        this.patientName = patientName;
+    }
+}
+
