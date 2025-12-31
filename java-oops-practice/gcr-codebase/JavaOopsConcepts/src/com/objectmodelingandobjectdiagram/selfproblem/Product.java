5257465ec1043e1c157d46820836a28e3bd1c953
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2025-12-31 14:57:26 +0530

    [Abhishek] : Added a program of Product class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelingandobjectdiagram/selfproblem/Product.java
@@ -0,0 +1,15 @@
+package com.objectmodelingandobjectdiagram.selfproblem;
+
+public class Product {
+
+    // product details
+    String productName;
+    double price;
+
+    // constructor
+    Product(String productName, double price) {
+        this.productName = productName;
+        this.price = price;
+    }
+}
+
