39991b1fea523ab783dc3a32c153437b9d666cd6
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-01-24 00:15:00 +0530

    [Abhishek] : Added a program to Censor Bad Words in a Sentence

--- java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/Regex/ReplaceAndModifyStrings/CensorWords.java
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/Regex/ReplaceAndModifyStrings/CensorWords.java
@@ -3,10 +3,10 @@ package com.Regex.ReplaceAndModifyStrings;
 public class CensorWords {
     public static void main(String[] args) {
 
-        String text = "I am an Engineer and I am learning the advance coding concepts.";
+        String text = "This is a damn bad example with some stupid words.";
 
         // Bad words list using OR (|)
-        String regex = "\\b(Engineer|learning)\\b";
+        String regex = "\\b(damn|stupid)\\b";
 
         // Replace bad words with ****
         String result = text.replaceAll(regex, "****");
