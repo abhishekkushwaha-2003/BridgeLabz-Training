74eab99a1c556dc80ae1274bf330f2d7df6cab1a
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-01-30 16:56:24 +0530

    [Abhishek] : Added a program to  Convert JSON to CSV and Vice Versa

--- java-iostream-practice/gcr-code-base/JavaInputOutputStreamsPractice/src/com/CSVDataHandling/AdvanceProblems/ConvertJSONtoCSVandViceVersa.java
+++ java-iostream-practice/gcr-code-base/JavaInputOutputStreamsPractice/src/com/CSVDataHandling/AdvanceProblems/ConvertJSONtoCSVandViceVersa.java
@@ -1,66 +1,26 @@
 package com.CSVDataHandling.AdvanceProblems;
 
-
-import java.io.BufferedWriter;
-import java.io.File;
+import com.fasterxml.jackson.databind.*;
 import java.io.FileWriter;
-import java.util.List;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConvertJSONtoCSVandViceVersa {
+    public static void main(String[] args) throws Exception {
 
- 
-    static class Student {
-        public int id;
-        public String name;
-        public int age;
-        public int marks;
-    }
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(new java.io.File("data/students.json"));
 
-    public static void main(String[] args) {
+        FileWriter writer = new FileWriter("output/updated_students.csv");
+        writer.write("ID,Name,Age,Marks\n");
 
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-
-          
-            List<Student> students = mapper.readValue(
-                new File("data/students.json"),
-                new TypeReference<List<Student>>() {}
+        for(JsonNode node : root) {
+            writer.write(
+                node.get("id").asInt() + "," +
+                node.get("name").asText() + "," +
+                node.get("age").asInt() + "," +
+                node.get("marks").asInt() + "\n"
             );
-
-            BufferedWriter bw =
-                new BufferedWriter(new FileWriter("data/students.csv"));
-
-           
-            bw.write("ID,Name,Age,Marks");
-            bw.newLine();
-
-            for (Student s : students) {
-                bw.write(
-                    s.id + "," +
-                    s.name + "," +
-                    s.age + "," +
-                    s.marks
-                );
-                bw.newLine();
-            }
-            bw.close();
-
-            System.out.println("JSON converted to CSV successfully");
-
-          
-            mapper.writerWithDefaultPrettyPrinter()
-                  .writeValue(
-                      new File("output/students_back.json"),
-                      students
-                  );
-
-            System.out.println("CSV converted back to JSON successfully");
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
+        writer.close();
     }
 }
