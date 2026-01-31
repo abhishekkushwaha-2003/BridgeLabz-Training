8183f82ce74c9fca5c72adc4135157e058222f95
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-01 02:33:53 +0530

    [Abhishek] : Added a program of Data Serialization for Backup

--- java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/FunctionalInterfaces/MarkerInterfaces/DataSerializationForBackup/BackupService.java
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/FunctionalInterfaces/MarkerInterfaces/DataSerializationForBackup/BackupService.java
@@ -1,5 +1,24 @@
 package com.FunctionalInterfaces.MarkerInterfaces.DataSerializationForBackup;
 
+import java.io.*;
+
 public class BackupService {
 
+    public static void backup(Object obj) throws Exception {
+
+        if (obj instanceof Serializable) {
+            ObjectOutputStream oos =
+                    new ObjectOutputStream(new FileOutputStream("backup.dat"));
+            oos.writeObject(obj);
+            oos.close();
+            System.out.println("Backup completed");
+        } else {
+            System.out.println("Object not serializable");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Employee emp = new Employee(101, "Rahul");
+        backup(emp);
+    }
 }
--- java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/FunctionalInterfaces/MarkerInterfaces/DataSerializationForBackup/Employee.java
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/FunctionalInterfaces/MarkerInterfaces/DataSerializationForBackup/Employee.java
@@ -1,5 +1,13 @@
 package com.FunctionalInterfaces.MarkerInterfaces.DataSerializationForBackup;
+import java.io.Serializable;
 
-public class Employee {
+public class Employee implements Serializable {
 
+    private int id;
+    private String name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
