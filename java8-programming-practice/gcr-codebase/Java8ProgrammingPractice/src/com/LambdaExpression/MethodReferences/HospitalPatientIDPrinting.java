d1cfa12614ab43fdf93c87b434ac2209d252f37b
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-01 01:17:22 +0530

    [Abhishek] : Added a program of Hospital Patient ID Printing

---
+++ java8-programming-practice/gcr-codebase/Java8ProgrammingPractice/src/com/LambdaExpression/MethodReferences/HospitalPatientIDPrinting.java
@@ -0,0 +1,12 @@
+package com.LambdaExpression.MethodReferences;
+
+import java.util.*;
+
+public class HospitalPatientIDPrinting {
+    public static void main(String[] args) {
+
+        List<Integer> patientIds = Arrays.asList(101, 102, 103, 104);
+
+        patientIds.forEach(System.out::println);
+    }
+}
