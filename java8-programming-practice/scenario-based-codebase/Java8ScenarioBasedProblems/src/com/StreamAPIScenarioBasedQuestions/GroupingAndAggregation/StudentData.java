409b7b3cb7aba8165e1ab12ebd52f510526d6d2f
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-06 12:59:41 +0530

    [Abhishek] : Updated code of StudentData.java class

--- java8-programming-practice/scenario-based-codebase/Java8ScenarioBasedProblems/src/com/StreamAPIScenarioBasedQuestions/GroupingAndAggregation/StudentData.java
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioBasedProblems/src/com/StreamAPIScenarioBasedQuestions/GroupingAndAggregation/StudentData.java
@@ -5,16 +5,14 @@ import java.util.*;
 public class StudentData {
     public static List<Student> getStudents() {
         return Arrays.asList(
-            new Student(1,"Abhishek",22,"Male","Mumbai","ComputerScience",45,Arrays.asList("111","222")),
-            new Student(2,"Amit",26,"Male","Delhi","ComputerScience",120,Arrays.asList("333")),
-            new Student(3,"Sneha",24,"Female","Pune","Electronics",30,Arrays.asList("444","555")),
-            new Student(4,"Rashi",21,"Female","Mumbai","ComputerScience",75,Arrays.asList("666")),
-            new Student(5,"Karan",21,"Male","Bangalore","Mechanical",15,Arrays.asList("777","888")),
-            new Student(6,"Palak",20,"Female","Chennai","Biotech",60,Arrays.asList("999")),
-            new Student(7,"Raj",23,"Male","Bangalore","Mechanical",90,Arrays.asList("101")),
-            new Student(8,"Simran",25,"Female","Delhi","Electronics",10,Arrays.asList("202","303"))
+            new Student(1,"Abhishek",22,"Male","Mumbai","ComputerScience",45,Arrays.asList("1117845692","2542165822")),
+            new Student(2,"Amit",26,"Male","Delhi","ComputerScience",120,Arrays.asList("3856974233")),
+            new Student(3,"Sneha",24,"Female","Pune","Electronics",30,Arrays.asList("4523148644","5558759632")),
+            new Student(4,"Rashi",21,"Female","Mumbai","ComputerScience",75,Arrays.asList("6667896542")),
+            new Student(5,"Karan",21,"Male","Bangalore","Mechanical",15,Arrays.asList("7785412367","8889876548")),
+            new Student(6,"Palak",20,"Female","Chennai","Biotech",60,Arrays.asList("9952369879")),
+            new Student(7,"Raj",23,"Male","Bangalore","Mechanical",90,Arrays.asList("1054789561")),
+            new Student(8,"Simran",25,"Female","Delhi","Electronics",10,Arrays.asList("2022145987","3589632503"))
         );
     }
 }
-
-
