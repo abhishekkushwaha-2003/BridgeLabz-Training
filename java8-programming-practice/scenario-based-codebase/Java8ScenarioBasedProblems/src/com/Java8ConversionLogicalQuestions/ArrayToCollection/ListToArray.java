40cc7bf66ba241c44f419e89a77b125dabfa96d1
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-02-07 23:55:06 +0530

    [Abhishek] : Added a program to Convert list to array

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioBasedProblems/src/com/Java8ConversionLogicalQuestions/ArrayToCollection/ListToArray.java
@@ -0,0 +1,14 @@
+package com.Java8ConversionLogicalQuestions.ArrayToCollection;
+
+import java.util.*;
+
+public class ListToArray {
+    public static void main(String[] args) {
+
+        List<String> list = List.of("A", "B", "C");
+
+        String[] arr = list.toArray(new String[0]);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
