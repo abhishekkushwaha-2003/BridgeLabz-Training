65d7496d0ca28cc59819b9b7e4f627f0965d8937
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-08-11 22:08:26 +0530

    [Abhishek] : Added Mockito test cases

---
+++ Day-8/contacts-app/src/main/java/com/contact/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.contact.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+
+		Map<String, String> errors = new HashMap<>();
+
+		exception.getBindingResult().getFieldErrors()
+				.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(ContactNotFoundException.class)
+	public ResponseEntity<Map<String, String>> handleContactNotFoundException(ContactNotFoundException exception) {
+
+		Map<String, String> error = new HashMap<>();
+
+		error.put("message", exception.getMessage());
+
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+}
---
+++ Day-8/contacts-app/src/main/java/com/contact/mapper/ContactMapper.java
@@ -0,0 +1,49 @@
+package com.contact.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.contact.dto.ContactRequestDTO;
+import com.contact.dto.ContactResponseDTO;
+import com.contact.entity.Contact;
+
+@Component
+public class ContactMapper {
+
+    public Contact toEntity(ContactRequestDTO request) {
+
+        Contact contact = new Contact();
+
+        contact.setFirstName(request.getFirstName());
+        contact.setLastName(request.getLastName());
+        contact.setEmail(request.getEmail());
+        contact.setPhone(request.getPhone());
+        contact.setAltPhone(request.getAltPhone());
+
+        return contact;
+    }
+
+    public ContactResponseDTO toResponseDTO(Contact contact) {
+
+        ContactResponseDTO response = new ContactResponseDTO();
+
+        response.setId(contact.getId());
+        response.setFirstName(contact.getFirstName());
+        response.setLastName(contact.getLastName());
+        response.setEmail(contact.getEmail());
+        response.setPhone(contact.getPhone());
+        response.setAltPhone(contact.getAltPhone());
+
+        return response;
+    }
+
+    public void updateEntity(
+            Contact contact,
+            ContactRequestDTO request) {
+
+        contact.setFirstName(request.getFirstName());
+        contact.setLastName(request.getLastName());
+        contact.setEmail(request.getEmail());
+        contact.setPhone(request.getPhone());
+        contact.setAltPhone(request.getAltPhone());
+    }
+}
