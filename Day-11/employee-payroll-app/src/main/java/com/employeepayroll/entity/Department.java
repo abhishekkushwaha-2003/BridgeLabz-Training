de5cf9471ebeaeb08237c2fb7b80dcb8811c56e6
Abhishek Kushwaha <kushwahaabhishek10k@gmail.com>
2026-08-17 00:18:24 +0530

    [Abhishek] : Updated Employee Payroll App with Spring Data JPA and added pagination & sorting and Spring Data JPA's Query By Example feature

--- Day-11/employee-payroll-app/src/main/java/com/employeepayroll/entity/Department.java
+++ Day-11/employee-payroll-app/src/main/java/com/employeepayroll/entity/Department.java
@@ -1,23 +1,17 @@
 package com.employeepayroll.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "department")
+@Getter
+@Setter
 public class Department {
 
     @Id
@@ -27,11 +21,4 @@ public class Department {
     private String name;
 
     private String description;
-
-    @OneToMany(
-        mappedBy = "department",
-        cascade = CascadeType.ALL,
-        orphanRemoval = true
-    )
-    private List<Employee> employees = new ArrayList<>();
 }
